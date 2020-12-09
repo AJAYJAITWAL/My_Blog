1d45c4d8e9d04e23a597c16e569a40cd082fe741
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2020-12-09 16:44:43 +0530

    Applies order in pagination in decending order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 
   def index
     # @articles = Article.all
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.paginate(page: params[:page], per_page: 5).order("created_at DESC")
   end
 
   def show
